220851184cba8eacaf37218dafce407d9207f45f
virinavam <viamear@gmail.com>
2025-04-03 20:09:47 +0500

    Connect filter mocks - Import the function for generation at the entry point - Pass the data to the component

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import ToolbarPresenter from './presenter/toolbar-presenter.js';
 import ContentPresenter from './presenter/content-presenter.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import TasksModel from './model/task-model.js';
+import {generateFilter} from './mock/filter-mock.js';
 
 const infoContainer = document.querySelector('.trip-main');
 const filterContainer = document.querySelector('.trip-controls__filters');
@@ -9,10 +10,14 @@ const contentContainer = document.querySelector('.trip-events');
 const tasksModel = new TasksModel();
 const boardPresenter = new BoardPresenter({ boardContainer: contentContainer, tasksModel });
 
+const filters = generateFilter(tasksModel.tasks); // Добавлено
+
 const toolbarPresenter = new ToolbarPresenter({
   filterContainer,
-  contentContainer
+  contentContainer,
+  filters // Передача фильтров
 });
+
 const contentPresenter = new ContentPresenter({
   infoContainer,
   contentContainer
