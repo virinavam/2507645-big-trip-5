ff521196bed7f6ff6d0246bb926adf59f04d8a65
virinavam <viamear@gmail.com>
2025-04-14 22:53:58 +0500

    Update const.js

--- src/const.js
+++ src/const.js
@@ -1,12 +1,3 @@
 const COLORS = ['black', 'yellow', 'blue', 'green', 'pink', 'red', 'purple'];
 
-const FilterType = {
-  ALL: 'all',
-  OVERDUE: 'overdue',
-  TODAY: 'today',
-  FAVORITES: 'favorites',
-  REPEATING: 'repeating',
-  ARCHIVE: 'archive',
-};
-
 export { COLORS };
