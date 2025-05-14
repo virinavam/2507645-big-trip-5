d0f5b57e50f0b50f4eff973d8e194632a1b31dcf
virinavam <viamear@gmail.com>
2025-05-14 15:54:08 +0300

    module8-task1

--- src/view/base-view.js
+++ src/view/base-view.js
@@ -3,14 +3,12 @@ class BaseView {
   getTemplate() {
     throw new Error('Not Implemented');
   }
-
   getElement() {
     if (!this.element) {
       this.element = createElement(this.getTemplate());
     }
     return this.element;
   }
-
   removeElement() {
     this.element = null;
   }
